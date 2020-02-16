3f61f2b5b71baf84a5ed1bb0284edf17bf2fadf7
Sandro Bürki <sandro.buerki@bbcag.ch>
2020-02-16 16:14:44 +0100

    print some info

--- create-release.sh
+++ create-release.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 echo "COMMIT_REF_NAME: '${CI_COMMIT_REF_NAME}'"
+echo "CI_JOB_TOKEN: '${CI_JOB_TOKEN}'"
+echo "CI_COMMIT_MESSAGE: '${CI_COMMIT_MESSAGE}'"
+echo "CI_PROJECT_ID: '${CI_PROJECT_ID}'"
+echo "CI_PROJECT_ID: '${CI_PROJECT_ID}'"
 
 curl \
     --header 'Content-Type: application/json' \
     --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
     --data "{ \
-        \"name\": \"BattleShips Release ${CI_COMMIT_REF_NAME}\", \
-        \"tag_name\": \"${CI_COMMIT_REF_NAME}\", \
-        \"description\": \"${CI_COMMIT_MESSAGE}\", \
-        \"assets\": { \
-            \"links\": \
+        'name': 'BattleShips Release ${CI_COMMIT_REF_NAME}', \
+        'tag_name': '${CI_COMMIT_REF_NAME}', \
+        'description': '${CI_COMMIT_MESSAGE}', \
+        'assets': { \
+            'links': \
             [{ \
-                \"name\": \"battleships.apk\", \
-                \"url\": \"https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/jobs/${CI_JOB_ID}/artifacts\" \
+                'name': 'battleships.apk', \
+                'url': 'https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/jobs/${CI_JOB_ID}/artifacts' \
             }] \
         } \
     }" \
