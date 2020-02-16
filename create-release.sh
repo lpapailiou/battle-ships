e7e7c6d384af7a1168250d5765675cec8772f5f4
Sandro Bürki <sandro.buerki@bbcag.ch>
2020-02-16 16:30:35 +0100

    trying with double quotes for json

--- create-release.sh
+++ create-release.sh
@@ -3,18 +3,19 @@
 set -x
 
 
+
 curl \
     --header 'Content-Type: application/json' \
     --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
     --data "{ \
-        'name': 'BattleShips Release ${CI_COMMIT_REF_NAME}', \
-        'tag_name': '${CI_COMMIT_REF_NAME}', \
-        'description': 'commit message', \
-        'assets': { \
-            'links': \
+        \"name\": \"BattleShips Release ${CI_COMMIT_REF_NAME}\", \
+        \"tag_name\": \"${CI_COMMIT_REF_NAME}\", \
+        \"description\": \"commit message\", \
+        \"assets\": { \
+            \"links\": \
             [{ \
-                'name': 'battleships.apk', \
-                'url': 'https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/jobs/${CI_JOB_ID}/artifacts' \
+                \"name\": \"battleships.apk\", \
+                \"url\": \"https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/jobs/${CI_JOB_ID}/artifacts\" \
             }] \
         } \
     }" \
