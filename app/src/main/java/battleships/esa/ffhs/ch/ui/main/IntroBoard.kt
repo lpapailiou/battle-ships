ef16d2112f7dfa4860813b9a91f45d4c97e8754a
Ramona Koksa <ramona.koksa@students.ffhs.ch>
2020-03-01 12:41:35 +0100

    Intro Screen

---
+++ app/src/main/java/battleships/esa/ffhs/ch/ui/main/IntroBoard.kt
@@ -0,0 +1,41 @@
+package battleships.esa.ffhs.ch.ui.main
+
+import android.widget.EditText
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity
+import battleships.esa.ffhs.ch.R
+import android.widget.Button
+import android.content.Intent
+
+class IntroBoard : AppCompatActivity(){
+
+    private var username: EditText? = null
+    private var train: Button? = null
+    private var online: Button? = null
+    private var score: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ic_introboard)
+
+        username = findViewById<EditText>(R.id.username)
+        train = findViewById<Button>(R.id.train)
+        online = findViewById<Button>(R.id.online)
+        score = findViewById<Button>(R.id.score)
+
+        train!!.setOnClickListener{
+            val intentTrain = Intent(this, BoardFragment::class.java) //TODO: Verify class MainFragment or BoardFragment?
+            startActivity(intentTrain)
+        }
+
+        online!!.setOnClickListener{
+            val intentOnline = Intent(this, BoardFragment::class.java) //TODO: Verify class MainFragment or BoardFragment?
+            startActivity(intentOnline)
+        }
+
+        score!!.setOnClickListener{
+            val intentScore = Intent(this, Score::class.java)
+        }
+
+    }
+}
---
+++ app/src/main/java/battleships/esa/ffhs/ch/ui/main/Score.kt
@@ -0,0 +1,8 @@
+package battleships.esa.ffhs.ch.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+
+class Score : AppCompatActivity(){
+
+
+}
