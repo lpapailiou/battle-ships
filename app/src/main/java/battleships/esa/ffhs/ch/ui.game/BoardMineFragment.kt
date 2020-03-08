5b22ca4449d41fd2237b1bdd0604b75be948da9e
lena.papailiou <lena.papailiou@students.ffhs.ch>
2020-03-08 01:26:25 +0100

    no idea how to add cells to board

--- app/src/main/java/battleships/esa/ffhs/ch/ui.game/BoardMineFragment.kt
+++ app/src/main/java/battleships/esa/ffhs/ch/ui.game/BoardMineFragment.kt
@@ -1,13 +1,20 @@
 package battleships.esa.ffhs.ch.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
+import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProviders
+import battleships.esa.ffhs.ch.MainActivity
 import battleships.esa.ffhs.ch.MainViewModel
 import battleships.esa.ffhs.ch.R
+import kotlinx.android.synthetic.main.board_mine_fragment.*
+
 
 class BoardMineFragment : Fragment() {
 
@@ -26,8 +33,27 @@ class BoardMineFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        //populateGrid()
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
+    // somehow add cells to board
+    private fun populateGrid() {
+        val gridParent = myBoardGrid
+        val mLocationsGrid = myBoardGrid
+        val nColumns = 14
+        val nCards: Int = 14
+        val fragmentManager: FragmentManager = (activity as MainActivity).getSupportFragmentManager()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        /*for (i in 0 until nCards) {
+            fragmentTransaction.add(
+                i,
+                (activity as MainActivity).findViewById(R.id.cell)
+            )
+        }*/
+        fragmentTransaction.commit()
+    }
+
 
 }
