0cbce276ec19f6b0490ad879e493bccb6b68e6b9
lpapailiou <lena@kaiki.ch>
2020-03-15 13:38:49 +0100

    added placeholder list to score view

--- app/src/main/java/battleships/esa/ffhs/ch/ui/score/ScoreFragment.kt
+++ app/src/main/java/battleships/esa/ffhs/ch/ui/score/ScoreFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import battleships.esa.ffhs.ch.R
 
@@ -18,6 +20,22 @@ class ScoreFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setList()
     }
+
+    // ----------------------------- list view for currently active games -----------------------------
+
+    // temporary handler for static gui testing
+    fun setList() {
+        val itemsAdapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(
+                (activity as MainActivity),
+                android.R.layout.simple_list_item_1,
+                resources.getStringArray(R.array.staticscoredata)
+            )
+        val listView: ListView = (activity as MainActivity).findViewById(R.id.scoreListId) as ListView
+        listView.setAdapter(itemsAdapter)
+    }
+
 
 }
