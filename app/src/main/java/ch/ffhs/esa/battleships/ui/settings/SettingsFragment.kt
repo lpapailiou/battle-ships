3411cc2f210da71d49136d50e96b22e14ffcef85
Ramona Koksa <ramona.koksa@students.ffhs.ch>
2020-05-24 15:15:23 +0200

    signout

--- app/src/main/java/ch/ffhs/esa/battleships/ui/settings/SettingsFragment.kt
+++ app/src/main/java/ch/ffhs/esa/battleships/ui/settings/SettingsFragment.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import ch.ffhs.esa.battleships.R
+import ch.ffhs.esa.battleships.event.Event
+import ch.ffhs.esa.battleships.ui.auth.LoginFragmentDirections
+import com.google.firebase.auth.FirebaseAuth
+import javax.inject.Inject
 
 class SettingsFragment : Fragment() {
+
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +25,23 @@ class SettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.settings_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val successObserver = Observer<Event<String>> {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
+        }
+
+        val failureObserver = Observer<Event<String>> {
+            Toast.makeText(requireContext(), it.getContentIfNotHandled(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun signOut(){
+        firebaseAuth.signOut()
     }
 
 
