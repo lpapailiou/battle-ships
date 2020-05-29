a7f7d4707657b977f3245fe032c6faf6babc9435
Sandro Bürki <sandro.buerki@bbcag.ch>
2020-05-29 11:10:53 +0200

    load ships locally*

--- app/src/main/java/ch/ffhs/esa/battleships/data/source/remote/ship/RemoteShipDataSource.kt
+++ app/src/main/java/ch/ffhs/esa/battleships/data/source/remote/ship/RemoteShipDataSource.kt
@@ -1,15 +1,22 @@
 package ch.ffhs.esa.battleships.data.source.remote.ship
 
 import ch.ffhs.esa.battleships.business.FIREBASE_BOARD_PATH
+import ch.ffhs.esa.battleships.business.FIREBASE_PLAYER_PATH
 import ch.ffhs.esa.battleships.data.DataResult
+import ch.ffhs.esa.battleships.data.game.Game
 import ch.ffhs.esa.battleships.data.ship.Ship
 import ch.ffhs.esa.battleships.data.ship.ShipDataSource
+import ch.ffhs.esa.battleships.data.source.remote.game.FirebaseGame
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
 
 class RemoteShipDataSource internal constructor(
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
@@ -23,7 +30,8 @@ class RemoteShipDataSource internal constructor(
                 return@withContext DataResult.Error(Exception("Ship does not have an Uid assigned"))
             }
 
-            val task = database.child(FIREBASE_BOARD_PATH).child(ship.boardUid!!).child("ship").child(ship.uid)
+            val task = database.child(FIREBASE_BOARD_PATH).child(ship.boardUid!!).child("ship")
+                .child(ship.uid)
                 .setValue(ship)
             task.await()
 
@@ -34,9 +42,45 @@ class RemoteShipDataSource internal constructor(
             return@withContext DataResult.Error(task.exception!!)
         }
 
-    override suspend fun loadByBoard(boardUid: String): DataResult<List<Ship>> {
-        TODO("Not yet implemented")
-    }
+    @ExperimentalCoroutinesApi
+    @InternalCoroutinesApi
+    override suspend fun loadByBoard(boardUid: String): DataResult<List<Ship>> =
+        withContext(ioDispatcher) {
 
+            val flow = callbackFlow<List<Ship>> {
+                val callback = object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                        val games = dataSnapshot.children
+                            .mapNotNull { it.getValue(Ship::class.java) }
+
+
+                        offer(games)
+                        channel.close()
+                    }
+
+                    override fun onCancelled(p0: DatabaseError) {
+                        throw p0.toException()
+                    }
+                }
+
+                database.child(FIREBASE_BOARD_PATH).child(boardUid).child("ship")
+                    .addListenerForSingleValueEvent(callback)
+
+                awaitClose { database.removeEventListener(callback) }
+
+                return@callbackFlow
+            }
+
+            var ships: List<Ship> = listOf()
+            flow.collect(object : FlowCollector<List<Ship>> {
+                override suspend fun emit(value: List<Ship>) {
+
+                    ships = value
+                }
+            })
+
+            return@withContext DataResult.Success(ships)
+        }
 
 }
