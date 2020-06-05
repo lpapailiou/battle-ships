36bacdf027f0af9e92da72008da45007bd8d79c2
Ramona Koksa <ramona.koksa@bluewin.ch>
2020-06-05 11:34:43 +0200

    mock dont work at this moment

--- app/src/test/java/ch/ffhs/esa/battleships/EmailLoginTest.kt
+++ app/src/test/java/ch/ffhs/esa/battleships/EmailLoginTest.kt
@@ -1,16 +1,25 @@
 package ch.ffhs.esa.battleships
 
+import android.app.Activity
 import androidx.lifecycle.ViewModel
+import ch.ffhs.esa.battleships.business.auth.EmailAuthModel
 import ch.ffhs.esa.battleships.business.auth.EmailAuthViewModel
 import ch.ffhs.esa.battleships.data.player.PlayerRepository
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
-
+import org.mockito.MockitoAnnotations
+import java.lang.Exception
+import java.util.concurrent.Executor
 
 class EmailLoginTest @javax.inject.Inject constructor(
-    private val playerRepository: PlayerRepository
 ) : ViewModel() {
 
     companion object {
@@ -19,30 +28,168 @@ class EmailLoginTest @javax.inject.Inject constructor(
         private const val UNDEF = 0
     }
 
+    private lateinit var successTask: Task<AuthResult>
+    private lateinit var failureTask: Task<AuthResult>
+
     @Mock
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mAuth: PlayerRepository
+    private lateinit var emailModel: EmailAuthModel
     private lateinit var logInModel: EmailAuthViewModel
 
     private var logInResult = UNDEF
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        successTask = object : Task<AuthResult>() {
+            override fun isComplete(): Boolean = true
+
+            override fun isSuccessful(): Boolean = true
+            // ...
+            override fun addOnCompleteListener(executor: Executor,
+                                               onCompleteListener: OnCompleteListener<AuthResult>
+            ): Task<AuthResult> {
+                onCompleteListener.onComplete(successTask)
+                return successTask
+            }
+
+            override fun getException(): Exception? {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnFailureListener(p0: OnFailureListener): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnFailureListener(
+                p0: Executor,
+                p1: OnFailureListener
+            ): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnFailureListener(
+                p0: Activity,
+                p1: OnFailureListener
+            ): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun getResult(): AuthResult? {
+                TODO("Not yet implemented")
+            }
+
+            override fun <X : Throwable?> getResult(p0: Class<X>): AuthResult? {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnSuccessListener(p0: OnSuccessListener<in AuthResult>): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnSuccessListener(
+                p0: Executor,
+                p1: OnSuccessListener<in AuthResult>
+            ): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnSuccessListener(
+                p0: Activity,
+                p1: OnSuccessListener<in AuthResult>
+            ): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun isCanceled(): Boolean {
+                TODO("Not yet implemented")
+            }
+        }
+
+        failureTask = object : Task<AuthResult>() {
+            override fun isComplete(): Boolean = true
+
+            override fun isSuccessful(): Boolean = false
+            // ...
+            override fun addOnCompleteListener(executor: Executor,
+                                               onCompleteListener: OnCompleteListener<AuthResult>): Task<AuthResult> {
+                onCompleteListener.onComplete(failureTask)
+                return failureTask
+            }
+
+            override fun getException(): Exception? {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnFailureListener(p0: OnFailureListener): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnFailureListener(
+                p0: Executor,
+                p1: OnFailureListener
+            ): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnFailureListener(
+                p0: Activity,
+                p1: OnFailureListener
+            ): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun getResult(): AuthResult? {
+                TODO("Not yet implemented")
+            }
+
+            override fun <X : Throwable?> getResult(p0: Class<X>): AuthResult? {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnSuccessListener(p0: OnSuccessListener<in AuthResult>): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnSuccessListener(
+                p0: Executor,
+                p1: OnSuccessListener<in AuthResult>
+            ): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun addOnSuccessListener(
+                p0: Activity,
+                p1: OnSuccessListener<in AuthResult>
+            ): Task<AuthResult> {
+                TODO("Not yet implemented")
+            }
+
+            override fun isCanceled(): Boolean {
+                TODO("Not yet implemented")
+            }
+        }
+        logInModel = EmailAuthViewModel(mAuth)
+    }
 
     @Test
     fun logInSuccess_test() {
         val email = "test@test.ch"
         val password = "123456"
-        Mockito.`when`(mAuth!!.signInWithEmailAndPassword(email, password))
+        Mockito.`when`(logInModel.signInWithEmailAndPassword(email, password))
             .thenReturn(successTask)
-        logInModel!!.logIn(email, password)
+        logInModel.signInWithEmailAndPassword(email, password)
         assert(logInResult == SUCCESS)
     }
 
 
     @Test
     fun logInFailure_test() {
-        val email = "cool@cool.com"
+        val email = "test@test.ch"
         val password = "123_456"
-        Mockito.`when`(mAuth!!.signInWithEmailAndPassword(email, password))
+        Mockito.`when`(logInModel.signInWithEmailAndPassword(email, password))
             .thenReturn(failureTask)
-        accountModel!!.logIn(email, password)
+        logInModel.signInWithEmailAndPassword(email, password)
         assert(logInResult == FAILURE)
     }
 
